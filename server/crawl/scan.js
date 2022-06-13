75e2897ac5a102ae91c6f13516334a6f6be9306b
Penjani Mkandawire <mkandawire15@gmail.com>
2022-06-13 15:06:51 +0200

    Fix

--- server/crawl/scan.js
+++ server/crawl/scan.js
@@ -127,7 +127,7 @@ const doScanJobs = async (site) => {
               });
             } else {
               logger.info("Response not okay.");
-              logger.info("Status: ", response.statusText());
+              logger.info("Status: " + response.statusText());
             }
           } catch (error) {
             console.log(error);
