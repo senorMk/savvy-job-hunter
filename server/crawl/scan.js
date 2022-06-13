2138063f1ad762d018d084701e4f1cd35e4d0e6b
Penjani Mkandawire <mkandawire15@gmail.com>
2022-06-13 14:37:37 +0200

    Add response status check.

--- server/crawl/scan.js
+++ server/crawl/scan.js
@@ -51,76 +51,83 @@ const doScanJobs = async (site) => {
               }
             );
 
-            let json = await response.json();
+            if (response.ok()) {
+              let json = await response.json();
+              let text = await response.text();
 
-            let html = json.html;
+              let html = json.html;
 
-            await page.setContent(html);
+              await page.setContent(html);
 
-            let Received = await page.evaluate((jobListingItem) => {
-              let Jobs = Array.from(
-                document.getElementsByClassName(jobListingItem)
-              );
+              let Received = await page.evaluate((jobListingItem) => {
+                let Jobs = Array.from(
+                  document.getElementsByClassName(jobListingItem)
+                );
 
-              let CleanArray = [];
+                let CleanArray = [];
 
-              Jobs.forEach(async (job) => {
-                let link = job.querySelector("a").getAttribute("href");
-                let companyLogo = job.querySelector("img").getAttribute("src");
-                let companyLogoAlt = job
-                  .querySelector("img")
-                  .getAttribute("alt");
-                let position = job
-                  .querySelector("div.position > h3")
-                  .innerHTML.replace(/&amp;/g, "&");
-                let companyName = job.querySelector(
-                  "div.position > div.company > strong"
-                ).innerHTML;
-                let location = job.querySelector("div.location").innerText;
-                let jobType = job.querySelector(
-                  "ul.meta > li.job-type"
-                ).innerText;
-                let dateOpen = job
-                  .querySelector("ul.meta > li.date > time")
-                  .getAttribute("datetime");
-                let verified = false;
+                Jobs.forEach(async (job) => {
+                  let link = job.querySelector("a").getAttribute("href");
+                  let companyLogo = job
+                    .querySelector("img")
+                    .getAttribute("src");
+                  let companyLogoAlt = job
+                    .querySelector("img")
+                    .getAttribute("alt");
+                  let position = job
+                    .querySelector("div.position > h3")
+                    .innerHTML.replace(/&amp;/g, "&");
+                  let companyName = job.querySelector(
+                    "div.position > div.company > strong"
+                  ).innerHTML;
+                  let location = job.querySelector("div.location").innerText;
+                  let jobType = job.querySelector(
+                    "ul.meta > li.job-type"
+                  ).innerText;
+                  let dateOpen = job
+                    .querySelector("ul.meta > li.date > time")
+                    .getAttribute("datetime");
+                  let verified = false;
 
-                let new_job = {
-                  link,
-                  companyLogo,
-                  companyLogoAlt,
-                  companyName,
-                  position,
-                  location,
-                  jobType,
-                  dateOpen,
-                  verified,
-                };
+                  let new_job = {
+                    link,
+                    companyLogo,
+                    companyLogoAlt,
+                    companyName,
+                    position,
+                    location,
+                    jobType,
+                    dateOpen,
+                    verified,
+                  };
 
-                if (jobType !== "Advert") {
-                  CleanArray.push(new_job);
-                }
-              });
-
-              return CleanArray;
-            }, jobListingItem);
-
-            Received.forEach((job) => {
-              // Lookup in database
-              JobsModel.findOne(
-                {
-                  link: job.link,
-                  companyName: job.companyName,
-                  position: job.position,
-                  date: Date.parse(job.date),
-                },
-                async function (err, job_res) {
-                  if (!job_res) {
-                    JobsModel.addJob(job);
+                  if (jobType !== "Advert") {
+                    CleanArray.push(new_job);
                   }
-                }
-              );
-            });
+                });
+
+                return CleanArray;
+              }, jobListingItem);
+
+              Received.forEach((job) => {
+                // Lookup in database
+                JobsModel.findOne(
+                  {
+                    link: job.link,
+                    companyName: job.companyName,
+                    position: job.position,
+                    date: Date.parse(job.date),
+                  },
+                  async function (err, job_res) {
+                    if (!job_res) {
+                      JobsModel.addJob(job);
+                    }
+                  }
+                );
+              });
+            } else {
+              logger.info("Response not okay.");
+            }
           } catch (error) {
             console.log(error);
             logger.error(error);
