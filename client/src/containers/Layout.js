2487d5f0f90a6818b3354e86ba161101edc54c50
Penjani Mkandawire <mkandawire15@gmail.com>
2022-07-13 23:29:53 +0200

    Improvement

--- client/src/containers/Layout.js
+++ client/src/containers/Layout.js
@@ -1,12 +1,12 @@
-import { Box, Container } from "@mui/material";
-import Header from "./Header";
-import React from "react";
+import { Box, Container } from '@mui/material';
+import Header from './Header';
+import React from 'react';
 
 const Layout = ({ children }) => {
   return (
     <Container maxWidth="false">
       <Header />
-      <Box>{children}</Box>
+      <Box sx={{ marginTop: '50px', marginBottom: '50px' }}>{children}</Box>
     </Container>
   );
 };
