a6995abae4ba8a8fdc2fd773a57e778385b58455
Penjani Mkandawire <mkandawire15@gmail.com>
2022-07-31 23:20:56 +0200

    Add env variable.

--- server/core/config/config.dev.js
+++ server/core/config/config.dev.js
@@ -1,16 +1,16 @@
-import "dotenv/config";
-import path, { dirname } from "path";
-import { fileURLToPath } from "url";
+import 'dotenv/config';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 let config = {};
 if (!process.env.NODE_ENV) {
-  process.env.NODE_ENV = "development";
+  process.env.NODE_ENV = 'development';
 }
-config.isProduction = process.env.NODE_ENV === "production";
+config.isProduction = process.env.NODE_ENV === 'production';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-config.logFileDir = path.join(__dirname, "../../log");
-config.logFileName = "app.log";
+config.logFileDir = path.join(__dirname, '../../log');
+config.logFileName = 'app.log';
 config.dbUser = process.env.dbUser;
 config.dbHost = process.env.dbHost;
 config.dbPass = process.env.dbPass;
@@ -24,8 +24,9 @@ config.serverPort = process.env.PORT;
 config.maxCrawlJobs = process.env.MAX_CRAWL_JOBS || 10;
 config.jobsPerPage = process.env.JOBS_PER_PAGE || 10;
 config.minsPerCrawl = process.env.MINS_PER_CRAWL || 10;
+config.crawlSite = process.env.CRAWL_SITE;
 config.frontDir = path.resolve(
-  path.join(__dirname, "../../../client/build/index.html")
+  path.join(__dirname, '../../../client/build/index.html')
 );
 
 export default config;
